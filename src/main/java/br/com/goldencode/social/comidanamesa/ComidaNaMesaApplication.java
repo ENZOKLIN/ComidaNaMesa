83d20498123182c2b58ca7ea11a93004bde09114
enzo.klin <enzo.klin@softplan.com.br>
2020-04-09 21:42:29 +0000

    Add project files.

---
+++ src/main/java/br/com/goldencode/social/comidanamesa/ComidaNaMesaApplication.java
@@ -0,0 +1,13 @@
+package br.com.goldencode.social.comidanamesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComidaNaMesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComidaNaMesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/goldencode/social/comidanamesa/ComidaNaMesaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goldencode.social.comidanamesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComidaNaMesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
